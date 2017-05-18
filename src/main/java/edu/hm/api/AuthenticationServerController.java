97b30d6ce1c69f72455aa7f9ef05900b1777ca7a
Lukas Marckmiller <marckmil@hm.edu>
2017-05-18 11:26:50 +0200

    init classes

---
+++ src/main/java/edu/hm/api/AuthenticationServerController.java
@@ -0,0 +1,14 @@
+package edu.hm.api; /*
+ * (C) 2017, Lukas Marckmiller, l.marckmiller@hm.edu on 18.05.2017.
+ * Java 1.8.0_121, Windows 10 Pro 64bit
+ * Intel Core i5-6600K CPU/3.50GHz overclocked 4.1GHz, 4 cores, 16000 MByte RAM)
+ * with IntelliJ IDEA 2017.1.1
+ */
+
+import com.auth0.jwt.JWT;
+
+public class AuthenticationServerController {
+    public AuthenticationServerController() {
+        //JWT.create().
+    }
+}
